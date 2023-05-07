fb2d2ecac38a1ad17415dab2f5a538701d287790
elimelech001 <aaron5yisroel@gmail.com>
2023-05-07 13:18:12 +0300

    cors for mac

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,12 @@ const Todo = require('./models/todo.model');
 
 const app = express();
 
-app.use(cors()) 
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
       username: 'postgres', // Your PostgreSQL username
       password: '1234', // Your PostgreSQL password
       database: 'postgres', // Your PostgreSQL database name
-      host: 'db', // Your PostgreSQL host
+      host: 'localhost', // Your PostgreSQL host
       port: 5432, // Your PostgreSQL port
       dialect: 'postgres',
       logging: false, // Disable logging
